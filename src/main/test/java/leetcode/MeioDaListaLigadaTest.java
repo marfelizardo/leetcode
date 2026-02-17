6e466f729955d50cabc4d5d140ae71a0bf31e052
Marco Felizardo <mar.felizardo@gmail.com>
2026-02-17 15:26:14 -0300

    Add JUnit 5 dependencies and configure Maven Surefire Plugin; rename test files

---
+++ src/main/test/java/leetcode/MeioDaListaLigadaTest.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class MeioDaListaLigadaTest {
+
+    @Test
+    void meioDaListaLigada() {
+
+        MeioDaListaLigada meioDaListaLigada = new MeioDaListaLigada();
+
+        //list node com valores [1,2,3,4,5]
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        meioDaListaLigada.middleNodeOne(head);
+
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next.next.next = new ListNode(8);
+        head.next.next.next.next.next.next.next.next = new ListNode(9);
+
+        meioDaListaLigada.middleNodeTwo(head);
+
+        head.next.next.next.next.next.next.next.next.next = new ListNode(10);
+
+        meioDaListaLigada.middleNodeTwo(head);
+
+    }
+}
